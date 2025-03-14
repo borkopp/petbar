4ee82b831c0c9430266702481a795c192f21d24e
Borko Petrevski <borko.petrevski@hs-augsburg.de>
2025-03-14 18:39:27 +0100

    Remove redundant ESLint rule configuration for React hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@react-hooks/exhaustive-deps": "off",
-    },
-  },
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
 
 export default eslintConfig;
